c357af88584a3ce65616c6ef2ef3ba058b29abe3
GheorgheStefan <stefan.gha9@gmail.com>
2026-03-27 10:17:14 +0200

    3 message

--- src/main/java/com/example/demo/LintingSampleService.java
+++ src/main/java/com/example/demo/LintingSampleService.java
@@ -23,34 +23,34 @@ public class LintingSampleService {
         return sum;
     }
 
-//     public int needBracesExample(final List<Integer> values) {
-//         int sum = 0;
-//
-//         for (final Integer value : values)
-//             if (value != null && value > 0)
-//                 sum += value;
-//
-//         while (sum < 10)
-//             sum++;
-//
-//         return sum;
-//     }
-//
-//     public void emptyBlockExample(final List<Integer> values) {
-//         if (values == null) {
-//         }
-//
-//         try {
-//             riskyCall();
-//         } catch (final RuntimeException ex) {
-//         }
-//
-//         try {
-//             riskyCall();
-//         } finally {
-//         }
-//     }
-//
-//     private void riskyCall() {
-//     }
+     public int needBracesExample(final List<Integer> values) {
+         int sum = 0;
+
+         for (final Integer value : values)
+             if (value != null && value > 0)
+                 sum += value;
+
+         while (sum < 10)
+             sum++;
+
+         return sum;
+     }
+
+     public void emptyBlockExample(final List<Integer> values) {
+         if (values == null) {
+         }
+
+         try {
+             riskyCall();
+         } catch (final RuntimeException ex) {
+         }
+
+         try {
+             riskyCall();
+         } finally {
+         }
+     }
+
+     private void riskyCall() {
+     }
 }
